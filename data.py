cdadbeaf930af981faa81a911d7709d4f1aa33d7
JulienFX <jujustor@gmail.com>
2025-08-23 12:27:51 +0200

    adaptation page d'accueil al-nassr

--- data.py
+++ data.py
@@ -1,28 +1,31 @@
 import pandas as pd 
 # ----- Données fictives -----
 
+team1 = "AL-NASSR"
+team2 = "AL-AHLI"
+
 # Match info
 match_info = {
-    "equipe1": "PSG",
-    "equipe2": "Barça",
-    "date": "2025-08-20 21:00",
-    "stade": "Parc des Princes",
-    "cote": "PSG 2.3 - Barça 3.1"
+    "equipe1": team1,
+    "equipe2": team2,
+    "date": "23-08-2025 14:00 🇫🇷",
+    "stade": "Hong Kong Stadium",
+    "cote": team1+" 2.1 - 2.9 "+ team2
 }
 
 # Stats équipes (buts marqués, encaissés, forme derniers 5 matchs en points)
 stats_equipes = pd.DataFrame({
-    "Equipe": ["PSG", "Barça"],
-    "Buts_Marques": [25, 22],
-    "Buts_Encaisses": [10, 15],
+    "Equipe": [team1, team2],
+    "Buts_Marques": [2, 5],
+    "Buts_Encaisses": [1, 1],
     "Forme_5_derniers": [4, 3]
 })
 
 # Stats joueurs (pour top buteurs, passeurs, dribbleurs)
 stats_joueurs = pd.DataFrame([
-    {"Nom": "Mbappé", "Equipe": "PSG", "Buts": 12, "Passes": 5, "Dribbles": 30},
-    {"Nom": "Messi", "Equipe": "PSG", "Buts": 8, "Passes": 10, "Dribbles": 25},
-    {"Nom": "Neymar", "Equipe": "PSG", "Buts": 7, "Passes": 8, "Dribbles": 40},
+    {"Nom": "Mbappé", "Equipe": team1, "Buts": 12, "Passes": 5, "Dribbles": 30},
+    {"Nom": "Messi", "Equipe": team1, "Buts": 8, "Passes": 10, "Dribbles": 25},
+    {"Nom": "Neymar", "Equipe": team1, "Buts": 7, "Passes": 8, "Dribbles": 40},
     {"Nom": "Lewandowski", "Equipe": "Barça", "Buts": 15, "Passes": 4, "Dribbles": 20},
     {"Nom": "Fati", "Equipe": "Barça", "Buts": 5, "Passes": 6, "Dribbles": 22},
     {"Nom": "Pedri", "Equipe": "Barça", "Buts": 3, "Passes": 9, "Dribbles": 18},
@@ -30,17 +33,17 @@ stats_joueurs = pd.DataFrame([
 
 # Lineup joueurs PSG et Barça (positions pour scatter plot)
 lineup = [
-  {"Nom": "Navas", "Equipe": "PSG", "Poste": "Gardien", "PosX": 10, "PosY": 50},
-  {"Nom": "Hakimi", "Equipe": "PSG", "Poste": "Défenseur Droit", "PosX": 20, "PosY": 80},
-  {"Nom": "Kimpembe", "Equipe": "PSG", "Poste": "Défenseur Central Droit", "PosX": 25, "PosY": 65},
-  {"Nom": "Marquinhos", "Equipe": "PSG", "Poste": "Défenseur Central Gauche", "PosX": 25, "PosY": 35},
-  {"Nom": "Nuno Mendes", "Equipe": "PSG", "Poste": "Défenseur Gauche", "PosX": 20, "PosY": 20},
-  {"Nom": "Verratti", "Equipe": "PSG", "Poste": "Milieu Central", "PosX": 40, "PosY": 50},
-  {"Nom": "Paredes", "Equipe": "PSG", "Poste": "Milieu Défensif", "PosX": 35, "PosY": 65},
-  {"Nom": "Gueye", "Equipe": "PSG", "Poste": "Milieu Offensif", "PosX": 35, "PosY": 35},
-  {"Nom": "Messi", "Equipe": "PSG", "Poste": "Ailier Droit", "PosX": 45, "PosY": 80},
-  {"Nom": "Mbappé", "Equipe": "PSG", "Poste": "Avant-centre", "PosX": 45, "PosY": 50},
-  {"Nom": "Neymar", "Equipe": "PSG", "Poste": "Ailier Gauche", "PosX": 45, "PosY": 20},
+  {"Nom": "Bento", "Equipe": team1, "Poste": "Gardien", "PosX": 10, "PosY": 50},
+  {"Nom": "Boushal", "Equipe": team1, "Poste": "Défenseur Droit", "PosX": 20, "PosY": 80},
+  {"Nom": "Simakan", "Equipe": team1, "Poste": "Défenseur Central Droit", "PosX": 20, "PosY": 60},
+  {"Nom": "Martinez", "Equipe": team1, "Poste": "Défenseur Central Gauche", "PosX": 20, "PosY": 40},
+  {"Nom": "Yahya", "Equipe": team1, "Poste": "Défenseur Gauche", "PosX": 20, "PosY": 20},
+  {"Nom": "Coman", "Equipe": team1, "Poste": "Milieu Droit", "PosX": 30, "PosY": 80},
+  {"Nom": "Al-Khaibari", "Equipe": team1, "Poste": "Milieu Central", "PosX": 30, "PosY": 60},
+  {"Nom": "brozovic", "Equipe": team1, "Poste": "Milieu Central", "PosX": 30, "PosY": 40},
+  {"Nom": "Mané", "Equipe": team1, "Poste": "Milieu Gauche", "PosX": 30, "PosY": 20},
+  {"Nom": "Félix", "Equipe": team1, "Poste": "Avant-centre", "PosX": 37.5, "PosY": 50},
+  {"Nom": "Ronaldo", "Equipe": team1, "Poste": "Ailier Gauche", "PosX": 45, "PosY": 50},
   {"Nom": "Ter Stegen", "Equipe": "Barça", "Poste": "Gardien", "PosX": 90, "PosY": 50},
   {"Nom": "Dest", "Equipe": "Barça", "Poste": "Défenseur Droit", "PosX": 80, "PosY": 80},
   {"Nom": "Piqué", "Equipe": "Barça", "Poste": "Défenseur Central Droit", "PosX": 75, "PosY": 65},
@@ -57,12 +60,12 @@ lineup = [
 ##data 
 # Données fictives pour les entraîneurs et remplaçants
 entraineurs = {
-    "PSG": "Christophe Galtier",
+    team1: "Jorge Jesus",
     "Barça": "Xavi Hernandez"
 }
 
 remplacants = {
-    "PSG": ["Ramos", "Donnarumma", "Soler", "Ekitike", "Sanches"],
+    team1: ["Al-Amri", "Al-Aqidi", "Al-Ghanam", "Al-Hassan", "Salem Al Najdi", "Al Nemer", "Aman", "Borges", "Wesley", "Gharib", "Haqawi", "Marran"],
     "Barça": ["Raphinha", "Torres", "Kessie", "Christensen", "Pena"]
 }
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from data import match_info, stats_joueurs, df_lineup,entraineurs,remplacants
+from data import match_info, stats_joueurs, df_lineup,entraineurs,remplacants, team1,team2
 
 
 # CSS - La partie qui permet de faire en sorte que l'affichage s'étale sur toute la largeur d'écran disponible
@@ -66,7 +66,7 @@ with tab1:
             x='PosX', y='PosY',
             color='Equipe', text='Nom',
             hover_data=['Poste'],
-            color_discrete_map={'PSG': 'blue', 'Barça': 'red'},
+            color_discrete_map={team1: 'blue', 'Barça': 'red'},
             height=500, width=700
         )
 
@@ -74,7 +74,7 @@ with tab1:
         fig.update_traces(
             textposition='top center',
             marker=dict(size=22, line=dict(width=2, color='black')),
-            textfont=dict(color='white', size=14)
+            textfont=dict(color='black', size=16)
         )
         fig.update_xaxes(
         range=[0, 100],
@@ -95,7 +95,7 @@ with tab1:
         fig.update_yaxes(autorange="reversed", range=[0, 100], showticklabels=False)
         fig.update_xaxes(range=[0, 100], showticklabels=False)
         fig.update_layout(
-            plot_bgcolor='#34C924',
+            plot_bgcolor='#F7F7F7',
             margin=dict(l=5, r=5, t=5, b=5),
         )
 
@@ -104,28 +104,28 @@ with tab1:
         # =====================
 
         # Contour du terrain
-        fig.add_shape(type="rect", x0=0, y0=0, x1=100, y1=100, line=dict(color="grey", width=3),layer="below")
+        fig.add_shape(type="rect", x0=0, y0=0, x1=100, y1=100, line=dict(color="#E3E3E3", width=3),layer="below")
 
         # Ligne médiane
-        fig.add_shape(type="line", x0=50, y0=0, x1=50, y1=100, line=dict(color="grey", width=2),layer="below")
+        fig.add_shape(type="line", x0=50, y0=0, x1=50, y1=100, line=dict(color="#E3E3E3", width=2),layer="below")
 
         # Rond central
-        fig.add_shape(type="circle", x0=40, y0=40, x1=60, y1=60, line=dict(color="grey", width=2),layer="below")
+        fig.add_shape(type="circle", x0=40, y0=40, x1=60, y1=60, line=dict(color="#E3E3E3", width=2),layer="below")
 
         # Point central
-        fig.add_shape(type="circle", x0=49.5, y0=49.5, x1=50.5, y1=50.5, fillcolor="grey", line=dict(color="white"),layer="below")
+        fig.add_shape(type="circle", x0=49.5, y0=49.5, x1=50.5, y1=50.5, fillcolor="#E3E3E3", line=dict(color="#E3E3E3"),layer="below")
 
         # Surfaces de réparation gauche et droite
-        fig.add_shape(type="rect", x0=0, y0=21, x1=18, y1=79, line=dict(color="grey", width=2),layer="below")
-        fig.add_shape(type="rect", x0=82, y0=21, x1=100, y1=79, line=dict(color="grey", width=2),layer="below")
+        fig.add_shape(type="rect", x0=0, y0=31, x1=10, y1=69, line=dict(color="#E3E3E3", width=2),layer="below")
+        fig.add_shape(type="rect", x0=90, y0=31, x1=100, y1=69, line=dict(color="#E3E3E3", width=2),layer="below")
 
         # Petites surfaces gauche et droite
-        fig.add_shape(type="rect", x0=0, y0=36, x1=6, y1=64, line=dict(color="grey", width=2),layer="below")
-        fig.add_shape(type="rect", x0=94, y0=36, x1=100, y1=64, line=dict(color="grey", width=2),layer="below")
+        fig.add_shape(type="rect", x0=0, y0=45, x1=2, y1=55, line=dict(color="#E3E3E3", width=2),layer="below")
+        fig.add_shape(type="rect", x0=98, y0=45, x1=100, y1=55, line=dict(color="#E3E3E3", width=2),layer="below")
 
-        # Points de penalty
-        fig.add_shape(type="circle", x0=11-0.5, y0=50-0.5, x1=11+0.5, y1=50+0.5, fillcolor="grey", line=dict(color="grey"),layer="below")
-        fig.add_shape(type="circle", x0=89-0.5, y0=50-0.5, x1=89+0.5, y1=50+0.5, fillcolor="grey", line=dict(color="grey"),layer="below")
+        # # Points de penalty
+        # fig.add_shape(type="circle", x0=11-0.5, y0=50-0.5, x1=11+0.5, y1=50+0.5, fillcolor="grey", line=dict(color="grey"),layer="below")
+        # fig.add_shape(type="circle", x0=89-0.5, y0=50-0.5, x1=89+0.5, y1=50+0.5, fillcolor="grey", line=dict(color="grey"),layer="below")
 
         st.plotly_chart(fig, use_container_width=True)
 
@@ -136,8 +136,8 @@ with tab1:
         col_entraineur1, col_entraineur2 = st.columns(2)
         
         with col_entraineur1:
-            st.markdown("**PSG**")
-            st.write(entraineurs["PSG"])
+            st.markdown("**AL-NASSR**")
+            st.write(entraineurs[team1])
         
         with col_entraineur2:
             st.markdown("**Barça**")
@@ -151,8 +151,8 @@ with tab1:
         col_remplacants1, col_remplacants2 = st.columns(2)
         
         with col_remplacants1:
-            st.markdown("**PSG**")
-            for remplacant in remplacants["PSG"]:
+            st.markdown(team1)
+            for remplacant in remplacants[team1]:
                 st.write(f"- {remplacant}")
         
         with col_remplacants2:
